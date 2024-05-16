933d26158222b2f33dba7cee02f145cf48e28afd
aurhid <aureliohideki@prof.educacao.sp.gov.br>
2024-05-16 13:04:42 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 15;
+
+let velocidadeXBolinha = 3;
+let velocidadeYBolinha = 3;
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    circle(xBolinha, yBolinha, diametro);
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+   if (xBolinha > width || xBolinha < 0) {
+    velocidadeXBolinha *= -1;}
+  if (yBolinha > height || yBolinha < 0) {
+        velocidadeYBolinha *= -1;
+    }}
+
